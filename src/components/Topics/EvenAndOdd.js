6f046665370316dea9d1d6c6bbe63b8b51d88067
Trillium <spiteless@gmail.com>
2020-07-21 15:59:21 -0700

    Completed Step 4

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -22,26 +22,20 @@ export default class EvenAndOdd extends Component {
         nums = nums.map( (n) => Number(n))
         nums = nums.filter( (n) => (n) ? true : (n === 0) ? true : false)
 
-        console.log(nums)
         for (let index in nums){
             let num = nums[index]
-            console.log(index, ":", num)
             if (num % 2 === 0) {
-                console.log("EVEN PUSH", num)
                 evens.push(num)}
             else {
-                console.log("EVEN PUSH", num)
                 odds.push(num)}
         }
-        console.log(evens)
-        console.log(odds)
         this.setState({ evenArray: evens, oddArray: odds, })
     }
         
     
     render (){
         return (
-            <div class="puzzleBox evenAndOddPB">
+            <div className="puzzleBox evenAndOddPB">
                 <h4> Evens and Odds </h4>
                 <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value)}/>
                 <button className="confirmationButton" onClick= { () => { this.assignEvenAndOdds(this.state.userInput) }}> Split </button>
--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
 
 export default class FilterObject extends Component{
+    constructor(){
+        super();
+        this.state = {
+            unFilteredArray: [],
+            userInput: '',
+            filteredArray: [],
+            
+            employees: [
+                {
+                name: 'Jimmy Joe',
+                title: 'Hack0r',
+                age: 12,
+                },
+                {
+                name: 'Jeremy Schrader',
+                age: 24,
+                hairColor: 'brown'
+                },
+                {
+                name: 'Carly Armstrong',
+                title: 'CEO',
+                }
+            ],
+        
+            userInput: '',
+            filteredEmployees: []
+        
+        }
+    }
+    handleChange(val) {
+        this.setState({ userInput: val})
+    }
+
+    filterEmployees(prop) {
+        let employees = this.state.employees
+        let filtered = []
+
+        filtered = employees.filter( (person) => person[prop] )
+
+        console.log(filtered)
+        
+        this.setState({ filteredEmployees: filtered})
+    }
+
     render (){
         return (
-        <p>Filter Objects</p>
+        
+    <div className="puzzleBox filterObjectPB">
+         <h4>Filter Object</h4>
+         <span className="puzzleText"> Original: { JSON.stringify(this.state.employees, null, 10) } </span>
+         <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value)}/>
+         <button className="confirmationButton" onClick= { () => { this.filterEmployees(this.state.userInput) }}> Filter </button>
+         <span className="resultsBox filterObjectRB"> Filtered: { JSON.stringify(this.state.filteredEmployees, null, 10)} </span>
+    </div>
         )
     }
+
 }
